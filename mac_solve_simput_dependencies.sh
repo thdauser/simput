1c07fa50c7658b05fc76279dd20b8ecf92504171
cucchetti <edoardo.cucchetti@irap.omp.eu>
2018-09-03 13:59:05 +0200

    Solving issues with mac dependencies script related to update of wcs

--- mac_solve_simput_dependencies.sh
+++ mac_solve_simput_dependencies.sh
@@ -11,9 +11,9 @@ for tool in bin/simputfile bin/simputimg bin/simputlc bin/simputmerge bin/simput
     bin/simputmultispec bin/simputpsd bin/simputrotate bin/simputspec bin/simputsrc bin/simputverify \
     lib/libsimput.dylib lib/libhdsp.dylib lib/libhdutils.dylib lib/libape.dylib \
     lib/libhdinit.dylib lib/libhdio.dylib lib/liblabnh.dylib lib/libposstring.dylib \
-    lib/libatFunctions.dylib
+    lib/libatFunctions.dylib lib/libCCfits.0.dylib
 do
-    for lib in libcfitsio.2.dylib libwcs.4.25.1.dylib
+    for lib in libcfitsio.2.dylib libwcs.5.19.1.dylib libwcs.4.25.1.dylib 
     do 
 	install_name_tool -change $lib $install_prefix/lib/$lib $install_prefix/$tool
     done
